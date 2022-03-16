a824f1ad2804ff5fb15a813f108438641d4ced5b
youssefsamy65 <99354955+youssefsamy65@users.noreply.github.com>
2022-03-16 15:11:23 -0700

    Add files via upload

---
+++ Main/main.h
@@ -0,0 +1,25 @@
+/*
+ * main.h
+ *
+ * Created: 2/28/2022 8:04:12 PM
+ *  Author: MOHAMED
+ */ 
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "Configuration.h"
+#include "Ext_Interrupt_Private.h"
+#include "ADC_Private.h"
+#include "LED0_Private.h"
+#include "LED1_Private.h"
+#include "LED2_Private.h"
+#include "Relay_Private.h"
+#include "Buzzer_Private.h"
+#include "SevenSegment_Private.h"
+#include "BTN0_Private.h"
+#include "Keypad_Private.h"
+#include "LCD_Private.h"
+
+#endif /* MAIN_H_ */
